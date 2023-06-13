56ed548b55458f9cdcbc836eba11a40a405a0588
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2023-06-13 22:58:54 +0700

    Create Python_dictionary.py

---
+++ Python_dictionary.py
@@ -0,0 +1,21 @@
+dictionary = {
+
+	"nama": "ikhsan",	"prodi": "informatika"
+
+}
+
+# menampilkan salah satu
+
+print(dictionary["nama"])
+
+print(dictionary["prodi"])
+
+print()
+
+#menampilkan semua
+
+for a,b in dictionary.items():
+
+	print(a)
+
+	print(b)
