ede3621227fd1d4049ea27fa8a44aff7833764c9
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2023-06-14 14:38:14 +0700

    Create Python_try_except.py

---
+++ Python_try_except.py
@@ -0,0 +1,6 @@
+try:
+    print(a)
+except:
+  # eror akan menampilkan ini karena fungsi try untuk menganalisis program apakah ada kesalahan
+  # karena di dalam try variabel a tidak ada maka akan masuk ke except
+    print('maaf program ada yang eror')
