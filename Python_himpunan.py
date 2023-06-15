87bf206083b65447ad4ac902a6766b06940c9fe3
NiaNyaa <109681800+NiaNyaa@users.noreply.github.com>
2023-06-15 09:39:19 +0700

    Create Python_himpunan.py

---
+++ Python_himpunan.py
@@ -0,0 +1,14 @@
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+# Gabungan (union) dari dua himpunan
+union_set = set1.union(set2)
+print(union_set)  # Output: {1, 2, 3, 4, 5}
+
+# Irisan (intersection) dari dua himpunan
+intersection_set = set1.intersection(set2)
+print(intersection_set)  # Output: {3}
+
+# Perbedaan (difference) antara dua himpunan
+difference_set = set1.difference(set2)
+print(difference_set)  # Output: {1, 2}
